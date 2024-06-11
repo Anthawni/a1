3a4cf6d7b408aee9293066e4e7ba4b1e5ee83751
Anthony Ngo <ango8208@sdsu.edu>
2024-06-10 20:13:08 -0700

    ls works cd does not

--- main.cpp
+++ main.cpp
@@ -1,116 +1,132 @@
-/* 
-main.cpp
-
-CS 480 Operating Systems
-Programming Assignment 1 / Chat Bot 
-Team Members 
-Name: Jeremiah Dobes
-RedID: 
-Edoras:
-
-Name: Anthony Ngo
-RedID: 825252396
-Edoras: cssc4410
-*/
-
-
-
 #include <iostream>
-#include <fstream>
-#include <unistd.h>
-#include <pthread.h>
-#include <semaphore.h>
-#include <memory>
+#include <string>
 #include <vector>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sstream> // Include <sstream> for std::istringstream
+#include <cstring>
+#include <cstdlib>
 
-#define THREAD_COUNT 7
-#define REPEAT_COUNT 8
+void execute_command(const std::vector<std::string>& commands);
+void parse_command(const std::string& command, std::vector<std::string>& args);
+void execute_single_command(const std::vector<std::string>& args);
+void execute_with_pipes(const std::vector<std::vector<std::string>>& commands);
 
-sem_t FLAG;
-
-void* thread_func(void* arg) {
-    // Get the ID of the current thread in order to perform operations
-    pthread_t tid = pthread_self();
-    int id = *static_cast<int*>(arg);
-
-    for (int i = 0; i < REPEAT_COUNT; i++) {
-        // Even threads sleep for 2 seconds, odd threads sleep for 3 seconds
-        if (id % 2 == 0) {
-            sleep(2);
-        } else {
-            sleep(3);
-        }
-
-        // Wait for the semaphore
-        sem_wait(&FLAG);
-
-        // Open the file and handle errors
-        std::ofstream file("QUOTE.txt", std::ios_base::app);
-        if (!file.is_open()) {
-            perror("Failed to open file");
-            sem_post(&FLAG);
-            pthread_exit(nullptr);
-        }
-
-        // Print to the file
-		if (id % 2 == 0) {
-            file << "Thread ID " << id << ": \"Controlling complexity is the essence of computer programming.\" --Brian Kernigan" << std::endl;
-        } else {
-            file << "Thread ID " << id << ": \"Computer science is no more about computers than astronomy is about telescopes.\" --Edsger Dijkstra" << std::endl;
-		}
-
-        // Print to stdout
-        std::cout << "Thread " << id << " is running" << std::endl;
-
-        // Release the semaphore
-        sem_post(&FLAG);
-    }
-
-    pthread_exit(nullptr);
-}
 
 int main() {
-    // Create and initialize the file
-    std::ofstream file("QUOTE.txt");
-    if (!file.is_open()) {
-        perror("Failed to create file");
-        exit(EXIT_FAILURE);
+    std::string input;
+
+    while (true) {
+        std::cout << "cssc4410% ";
+        std::getline(std::cin, input);
+
+        if (input == "exit") {
+            break;
+        } 
+
+        std::vector<std::vector<std::string>> commands;
+        size_t pos = 0;
+        std::string token;
+        while ((pos = input.find('|')) != std::string::npos) {
+            token = input.substr(0, pos);
+            input.erase(0, pos + 1);
+            std::vector<std::string> args;
+            parse_command(token, args);
+            commands.push_back(args);
+        }
+        std::vector<std::string> args;
+        parse_command(input, args);
+        commands.push_back(args);
+
+        execute_with_pipes(commands);
     }
 
-    // Write the process ID
-    file << "Process ID: " << getpid() << std::endl;
-    file.close();
+    return 0;
+}
 
-    // Initialize the semaphore
-    if (sem_init(&FLAG, 0, 1) != 0) {
-        perror("Failed to initialize semaphore");
-        exit(EXIT_FAILURE);
+void execute_command(const std::vector<std::string>& commands) {
+    const char** args = new const char*[commands.size() + 1];
+    for (size_t i = 0; i < commands.size(); ++i) {
+        args[i] = commands[i].c_str();
     }
+    args[commands.size()] = NULL;
 
-    // Create threads
-    std::vector<pthread_t> threads(THREAD_COUNT);
-    for (int i = 0; i < THREAD_COUNT; i++) {
-        int *arg = new int(i); // Allocate memory for thread argument
-        // Handle thread creation failure
-        std::cout << "Creating thread, in main(): " << *arg << std::endl;
-        if (pthread_create(&threads[i], nullptr, thread_func, arg) != 0) {
-            perror("Failed to create thread");
+    execvp(args[0], const_cast<char* const*>(args));
+    perror("execvp");
+    exit(EXIT_FAILURE);
+}
+
+void parse_command(const std::string& command, std::vector<std::string>& args) {
+    std::istringstream iss(command);
+    std::string token;
+    while (iss >> token) {
+        args.push_back(token);
+    }
+}
+
+void execute_single_command(const std::vector<std::string>& args) {
+    pid_t pid = fork();
+    if (pid < 0) {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    } else if (pid == 0) { 
+        execute_command(args);
+    } else { // Parent process
+        wait(NULL);
+    }
+}
+
+void execute_with_pipes(const std::vector<std::vector<std::string>>& commands) {
+    int num_commands = commands.size();
+    int pipefds[num_commands - 1][2]; 
+
+    // make pipes
+    for (int i = 0; i < num_commands - 1; ++i) {
+        if (pipe(pipefds[i]) == -1) {
+            perror("pipe");
             exit(EXIT_FAILURE);
         }
     }
 
-    // Wait for all threads to complete
-    for (auto& thread : threads) {
-        pthread_join(thread, nullptr);
+    // Execute commands
+    for (int i = 0; i < num_commands; ++i) {
+        pid_t pid = fork();
+        if (pid == -1) {
+            perror("fork");
+            exit(EXIT_FAILURE);
+        } else if (pid == 0) { 
+            // Set up pipes
+            if (i != 0) {
+                if (dup2(pipefds[i - 1][0], STDIN_FILENO) == -1) {
+                    perror("dup2");
+                    exit(EXIT_FAILURE);
+                }
+            }
+            if (i != num_commands - 1) {
+                if (dup2(pipefds[i][1], STDOUT_FILENO) == -1) {
+                    perror("dup2");
+                    exit(EXIT_FAILURE);
+                }
+            }
+
+            // Close pipe file descriptors
+            for (int j = 0; j < num_commands - 1; ++j) {
+                close(pipefds[j][0]);
+                close(pipefds[j][1]);
+            }
+
+            execute_command(commands[i]);
+        }
     }
 
-    // Destroy the semaphore
-    if (sem_destroy(&FLAG) != 0) {
-        perror("Failed to destroy semaphore");
-        exit(EXIT_FAILURE);
+    // Close pipe file desc
+    for (int i = 0; i < num_commands - 1; ++i) {
+        close(pipefds[i][0]);
+        close(pipefds[i][1]);
     }
 
-    // Exit the code
-    std::cout << "All threads have completed. Exiting..." << std::endl;
-    return 0;
+    // Wait for all child processes to finish
+    for (int i = 0; i < num_commands; ++i) {
+        wait(NULL);
+    }
 }
